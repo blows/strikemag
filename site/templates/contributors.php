0eb10d50579f32c80614932dcb110fe48746e8dc
Hannah Blows <ha.blows@gmail.com>
2017-03-27 21:37:37 +0100

    Alphabetise sort and layout contributors page

--- site/templates/contributors.php
+++ site/templates/contributors.php
@@ -2,32 +2,55 @@
 
   <main class="main" role="main">
 
+    <header class="contributors-intro">
+      <h1>Join our <?php echo $page->children()->count() ?> published writers and artists, send us a pitch!</h1>
+    </header>
+
+<div class="contributors-flex-center">
     <div class="contributors">
 
-      <header class="">
-        <h1>Join our <?php echo $page->children()->count() ?> published writers and artists, send us a pitch!</h1>
-      </header>
+      <?php $alphabetise = alphabetise($page->children()->sortby('title'), array('key' => 'title')); ?>
 
-      <div class="">
-        <?= $page->text()->kirbytext() ?>
-      </div>
+      <?php foreach($alphabetise as $letter => $items): ?>
+        <h4 class="alphabet-letter"><?php echo strtoupper($letter) ?></h4>
+          
+            <?php foreach($items as $contributor): ?>
+              <article class="contributor" id="<?php echo $contributor->uid() ?>">
+                <?php if ($contributor->isVisible()): ?>
+                  <a href="<?php echo $contributor->url() ?>">
+                <?php endif ?>
 
-      <?php foreach($page->children() as $contributor): ?>
-      <article class="contributor" id="<?php echo $contributor->uid() ?>">
+                <div class="contributor-wrap">
 
-        <figure class="contributor-profilepic">
-          <img src="<?php echo $contributor->profilepic() ?>" alt="<?php echo $contributor->title()->html() ?>">
-        </figure>
+                  <?php if ($contributor->profilepic()->isNotEmpty()): ?>
+                    <figure class="contributor-profilepic">
+                      <img src="<?php echo $contributor->profilepic()->toFile()->focusCrop(150, 150)->url() ?>" alt="<?php echo $contributor->title()->html() ?>">
+                    </figure>
+                  <?php else: ?>
+                    <div class="contributor-profilepic" style="background-color: <?php $colors = $page->colors()->split(','); $shuffle = a::shuffle($colors); echo $color = a::first($shuffle); ?>"></div>
+                  <?php endif; ?>
 
-        <h1 class="contributor-name"><a href="<?php echo $contributor->url() ?>"><?php echo $contributor->title()->html() ?><a/></h1>
+                  <div class="contributor-info">
+                    <h1 class="contributor-name"><?php echo $contributor->title()->upper()->html() ?></h1>
+                    <ul class="contributor-role">
+                      <?php foreach($contributor->role()->split(',') as $role): ?>
+                      <li><?php echo $role ?></li>
+                      <?php endforeach ?>
+                    </ul>
+                  </div>
 
-        <p class="contributor-bio"><?php echo $contributor->bio()->html() ?></p>
+                </div>
 
-      </article>
-      </a>
+                <?php if ($contributor->isVisible()): ?>
+                  </a>
+                <?php endif ?>
+              </article>
+           	<?php endforeach ?>
+
       <?php endforeach ?>
 
     </div>
+  </div>
 
   </main>
 
