3cbbbcd557d8a2eb57ce0b9618691d7db241f734
Hannah Blows <hannnarr@gmail.com>
2017-06-16 23:56:15 +0100

    Restyle footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,38 +1,24 @@
   <footer class="footer group" role="contentinfo">
-    <div class="footer-left">
-      <ul>
-      <?php foreach($site->find('home', 'magazine', 'contributors') as $subpage): ?>
-        <li>
-          <a href="<?php echo $subpage->url() ?>">
-          <?php echo html($subpage->title()->upper()) ?>
-          </a>
-        </li>
-        <?php endforeach ?>
-        <li class="">
-          <a href="http://www.strikemag.bigcartel.com">SHOP</a>
-        </li>
-      </ul>
-    </div>
-    <div class="footer-right">
-      <ul>
-      <?php foreach($site->find('home', 'magazine') as $subpage): ?>
-        <li>
-          <a href="<?php echo $subpage->url() ?>">
-          <?php echo html($subpage->title()->upper()) ?>
-          </a>
-        </li>
-        <?php endforeach ?>
-        <li class="">
-          <a href="http://www.strikemag.org" target="_blank">FB</a>
-          <a href="http://www.strikemag.org" target="_blank">TW</a>
-          <a href="http://www.strikemag.org" target="_blank">IG</a>
-        </li>
-      </ul>
-    </div>
+
     <div class="footer-newsletter">
       <p>Sign up to our irregular newsletter receive our irregular newsletter updates</p>
       <?php snippet('newsletter') ?>
     </div>
+
+    <div class="footer-nav">
+      <ul class="footer-links">
+        <li><a href="http://www.strikemag.org" target="_blank">Contact</a></li>
+        <li><a href="http://www.strikemag.org" target="_blank">Subscribe</a></li>
+        <li><a href="http://www.strikemag.org" target="_blank">Contributors</a></li>
+      </ul>
+
+      <ul class="footer-socials">
+        <li><a href="http://www.strikemag.org" target="_blank">FB</a></li>
+        <li><a href="http://www.strikemag.org" target="_blank">TW</a></li>
+        <li><a href="http://www.strikemag.org" target="_blank">IG</a></li>
+      </ul>
+    </div>
+
   </footer>
 
   <?= js('assets/js/jquery.min.js') ?>
