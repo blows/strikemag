76d7fc4a2d03b8840fe4f737623ba8a5cf5f8195
Hannah Blows <hannnarr@gmail.com>
2017-06-17 23:45:46 +0100

    Style info page

--- site/templates/info.php
+++ site/templates/info.php
@@ -1,45 +1,68 @@
 <?php snippet('header') ?>
 
-<?php snippet('aboutmenu') ?>
+  <main class="info main text" role="main">
 
-  <main class="about main text" role="main">
+    <section class="info-nav">
+      <a href="#goabout" class="button">ABOUT</a>
+      <a href="#gocontact" class="button">CONTACT</a>
+      <a href="#gocontribute" class="button">CONTRIBUTE</a>
+      <a href="#godistribute" class="button">DISTRIBUTE</a>
+      <a href="#gostockists" class="button">STOCKISTS</a>
+    </section>
 
-    <article class="about-text">
-      <section class="about-section">
-        <h1>ABOUT US</h1>
-        <p><?= $page->aboutus()->kirbytext() ?></p>
+    <article class="info-text">
+
+      <div class="info-jump" id="goabout"></div>
+      <section class="info-section group" id="about">
+        <div class="info-section__wrap">
+          <h2>ABOUT</h2>
+          <?= $page->aboutus()->kirbytext() ?>
+        </div>
       </section>
 
-      <section class="about-section">
-        <h2>CONTACT</h2>
-        <p><?= $page->contact()->kirbytext() ?></p>
+      <div class="info-jump" id="gocontact"></div>
+      <section class="info-section group" id="contact">
+        <div class="info-section__wrap">
+          <h2>CONTACT</h2>
+          <?= $page->contact()->kirbytext() ?>
+        </div>
       </section>
 
-      <section class="about-section">
-        <h2>CONTRIBUTE</h2>
-        <p><?= $page->submissions()->kirbytext() ?></p>
+      <div class="info-jump" id="gocontribute"></div>
+      <section class="info-section group" id="contribute">
+        <div class="info-section__wrap">
+          <h2>CONTRIBUTE</h2>
+          <?= $page->submissions()->kirbytext() ?>
+        </div>
       </section>
 
-      <section class="about-section">
-        <h2>DISTRIBUTE</h2>
-        <p><?= $page->distribution()->kirbytext() ?></p>
+      <div class="info-jump" id="godistribute"></div>
+      <section class="info-section group" id="distribute">
+        <div class="info-section__wrap">
+          <h2>DISTRIBUTE</h2>
+          <?= $page->distribution()->kirbytext() ?>
+        </div>
       </section>
 
-      <section class="about-section">
-        <h2>STOCKISTS</h2>
-        <ul class="stockists">
-          <?php foreach($page->stockists()->toStructure() as $stockist): ?>
-            <a href="<?= $stockist->link()->url() ?>" target="_blank">
-              <li class="stockists_stock">
-                <div>
-                  <p class="stockists_stockist"><?= $stockist->stockist()->html() ?></p>
-                  <p class="stockists_country"><?= $stockist->location()->html() ?></p>
-                </div>
-              </li>
-            </a>
-          <?php endforeach ?>
-        </ul>
+      <div class="info-jump" id="gostockists"></div>
+      <section class="info-section group" id="stockists">
+        <div class="info-section__wrap">
+          <h2>STOCKISTS</h2>
+          <ul class="stockists">
+            <?php foreach($page->stockists()->toStructure() as $stockist): ?>
+                <li class="stockists_stock">
+                  <a href="<?= $stockist->link()->url() ?>" target="_blank">
+                    <div>
+                      <p class="stockists_stockist"><?= $stockist->stockist()->upper()->html() ?></p>
+                      <p class="stockists_location"><?= $stockist->location()->html() ?></p>
+                    </div>
+                  </a>
+                </li>
+            <?php endforeach ?>
+          </ul>
+        </div>
       </section>
+
     </article>
 
   </main>
