b193526097ef89457563deea040175a37b943ddf
Hannah Blows <hannnarr@gmail.com>
2017-09-25 12:50:57 +0100

    Restyle support bar

--- site/snippets/support.php
+++ site/snippets/support.php
@@ -3,6 +3,5 @@
 Help us keep publishing the important work we care about</p>
   <div class="support-bar__buttons">
     <a class="support-bar__button button">SUBSCRIBE</a>
-    <a class="support-bar__button button">DONATE</a>
   </div>
 </div>
