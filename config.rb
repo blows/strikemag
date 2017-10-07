4f7faad0aa410424bab9de0ccbac155af58a1632
Hannah Blows <hannnarr@gmail.com>
2017-10-07 20:52:23 +0100

    Compress css output

--- config.rb
+++ config.rb
@@ -9,8 +9,8 @@ images_dir = "assets/images"
 javascripts_dir = "assets/js"
 sourcemap = true
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+# You can select your preferred output style here (can be overridden via the command line): :expanded or :nested or :compact or
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
