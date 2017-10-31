5e4d2b779d26a38b6eef47440459c072579d226b
Hannah Blows <hannnarr@gmail.com>
2017-10-31 00:16:56 +0000

    Title case Info page

--- site/templates/info.php
+++ site/templates/info.php
@@ -3,11 +3,11 @@
   <main class="info main text" role="main">
 
     <section class="info-nav">
-      <a href="#goabout" class="button">ABOUT</a>
-      <a href="#gocontact" class="button">CONTACT</a>
-      <a href="#gocontribute" class="button">CONTRIBUTE</a>
-      <a href="#godistribute" class="button">DISTRIBUTE</a>
-      <a href="#gostockists" class="button">STOCKISTS</a>
+      <a href="#goabout" class="button">About</a>
+      <a href="#gocontact" class="button">Contact</a>
+      <a href="#gocontribute" class="button">Contribute</a>
+      <a href="#godistribute" class="button">Distribute</a>
+      <a href="#gostockists" class="button">Stockists</a>
     </section>
 
     <article class="info-text">
@@ -15,7 +15,7 @@
       <div class="info-jump" id="goabout"></div>
       <section class="info-section group" id="about">
         <div class="info-section__wrap">
-          <h2>ABOUT</h2>
+          <h2>About</h2>
           <?= $page->aboutus()->kirbytext() ?>
         </div>
       </section>
@@ -23,7 +23,7 @@
       <div class="info-jump" id="gocontact"></div>
       <section class="info-section group" id="contact">
         <div class="info-section__wrap">
-          <h2>CONTACT</h2>
+          <h2>Contact</h2>
           <?= $page->contact()->kirbytext() ?>
         </div>
       </section>
@@ -31,7 +31,7 @@
       <div class="info-jump" id="gocontribute"></div>
       <section class="info-section group" id="contribute">
         <div class="info-section__wrap">
-          <h2>CONTRIBUTE</h2>
+          <h2>Contribute</h2>
           <?= $page->submissions()->kirbytext() ?>
         </div>
       </section>
@@ -39,7 +39,7 @@
       <div class="info-jump" id="godistribute"></div>
       <section class="info-section group" id="distribute">
         <div class="info-section__wrap">
-          <h2>DISTRIBUTE</h2>
+          <h2>Distribute</h2>
           <?= $page->distribution()->kirbytext() ?>
         </div>
       </section>
@@ -47,7 +47,7 @@
       <div class="info-jump" id="gostockists"></div>
       <section class="info-section group" id="stockists">
         <div class="info-section__wrap">
-          <h2>STOCKISTS</h2>
+          <h2>Stockists</h2>
           <ul class="stockists">
             <?php foreach($page->stockists()->toStructure() as $stockist): ?>
                 <li class="stockists_stock">
