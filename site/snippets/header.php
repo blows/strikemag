aa8e8899a460014195d3fb4aea56d2ae480dde96
Hannah Blows <hannnarr@gmail.com>
2017-11-18 00:48:11 +0000

    Add Piwik Tracking

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -69,6 +69,25 @@
   <!-- Font Awesome -->
   <script async src="https://use.fontawesome.com/6a356015ff.js"></script>
 
+  <!-- Piwik -->
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+    _paq.push(["setCookieDomain", "*.strikemag.org"]);
+    _paq.push(["setDomains", ["*.strikemag.org","*.strikemag.org","*.www.strikemag.org","*.www.strikemag.org"]]);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//www.thisblows.uk/analytics/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', '5']);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <!-- End Piwik Code -->
+
 </head>
 <body>
 
